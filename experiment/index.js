49a51eb92f197d13f2b85f9b3587ae08e85e14b1
Jarid Schoenlein <jrschoenlein@gmail.com>
2021-10-07 19:46:59 -0700

    Add files via upload

---
+++ experiment/index.js
@@ -0,0 +1,19 @@
+const changer = document.querySelector("div.color input")
+const bodyTag = document.querySelector("body")
+
+changer.addEventListener("input", function () {
+    bodyTag.style.backgroundColor = changer.value
+
+
+  
+  const color = chroma(changer.value)
+  
+  if (color.luminance() < 0.3) {
+    bodyTag.classList.add("dark")
+  } else {
+    bodyTag.classList.remove("dark")
+  }
+
+
+})
+
